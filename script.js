e82e871a2cf9a35d0241dbe4755c5ce609e092e3
Shangbingj <33359344+Shangbingj@users.noreply.github.com>
2024-07-07 20:39:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+const cocktailIcon = L.icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/512/920/920539.png',
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+    popupAnchor: [0, -30]
+});
+
 cocktails.forEach(cocktail => {
-    L.marker([cocktail.lat, cocktail.lng])
+    L.marker([cocktail.lat, cocktail.lng], {icon: cocktailIcon})
         .addTo(map)
         .bindPopup(`
-            <h3>${cocktail.name}</h3>
-            <p><strong>Origin:</strong> ${cocktail.origin}</p>
-            <p><strong>Year:</strong> ${cocktail.year}</p>
-            <p><strong>Ingredients:</strong> ${cocktail.ingredients}</p>
-            <p><strong>History:</strong> ${cocktail.history}</p>
+            <div class="cocktail-popup">
+                <h3>${cocktail.name}</h3>
+                <p><strong>Origin:</strong> ${cocktail.origin}</p>
+                <p><strong>Year:</strong> ${cocktail.year}</p>
+                <p><strong>Ingredients:</strong> ${cocktail.ingredients}</p>
+                <p><strong>History:</strong> ${cocktail.history}</p>
+            </div>
         `);
 });
